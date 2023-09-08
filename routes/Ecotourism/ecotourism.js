5358b28adade53e02f1aa66ea29c6040284ea3ef
ebenezer710 <ebenezerholz710@gmail.com>
2023-09-08 16:27:41 -0300

    :wrench: Near_ecotourism Post

--- routes/Ecotourism/ecotourism.js
+++ routes/Ecotourism/ecotourism.js
@@ -57,4 +57,33 @@ router.post("/near_ecotourism/:latitude/:longitude/:dist", async (req, res) => {
 	}
 });
 
+router.post("/near_ecotourism/:latitude/:longitude/:dist", async (req, res) => {
+	try {
+		const { latitude, longitude } = req.params;
+
+		if (!latitude || !longitude) {
+			return res.status(400).json({ error: "Latitude and longitude are required." });
+		}
+
+		const latitudeReferencia = parseFloat(latitude);
+		const longitudeReferencia = parseFloat(longitude);
+
+		const ecotourismPlaces = await Ecotourism.find();
+
+		const ecotourismPlacesNearby = ecotourismPlaces.filter((place) => {
+			const distancia = getDistance(
+				{ latitude: latitudeReferencia, longitude: longitudeReferencia },
+				{ latitude: place.localisacao_geografica.coordinates[0], longitude: place.localisacao_geografica.coordinates[1] }
+			);
+			console.log(req.params.dist);
+			return distancia <= req.params.dist * 1000;
+		});
+
+		res.status(200).json({ ecotourismPlacesNearby });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: err.message });
+	}
+});
+
 module.exports = router;
