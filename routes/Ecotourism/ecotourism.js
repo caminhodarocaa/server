575ebae3c4c4d98072f7f89a6674ce45ae8d9118
ebenezer710 <ebenezerholz710@gmail.com>
2023-09-08 16:25:41 -0300

    :chart_with_upwards_trend: Near_ecotourism Post

--- routes/Ecotourism/ecotourism.js
+++ routes/Ecotourism/ecotourism.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ObjectId = require("mongoose").Types.ObjectId;
+const { getDistance } = require("geolib");
 
 const router = express.Router();
 
@@ -23,6 +24,35 @@ router.post("/", async (req, res) => {
 
 		res.json({ message: "new Ecotourism place created successfully" });
 	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+router.post("/near_ecotourism/:latitude/:longitude/:dist", async (req, res) => {
+	try {
+		const { latitude, longitude } = req.params;
+
+		if (!latitude || !longitude) {
+			return res.status(400).json({ error: "Latitude and longitude are required." });
+		}
+
+		const latitudeReferencia = parseFloat(latitude);
+		const longitudeReferencia = parseFloat(longitude);
+
+		const ecotourismPlaces = await Ecotourism.find();
+
+		const ecotourismPlacesNearby = ecotourismPlaces.filter((place) => {
+			const distancia = getDistance(
+				{ latitude: latitudeReferencia, longitude: longitudeReferencia },
+				{ latitude: place.localisacao_geografica.coordinates[0], longitude: place.localisacao_geografica.coordinates[1] }
+			);
+			console.log(req.params.dist);
+			return distancia <= req.params.dist * 1000;
+		});
+
+		res.status(200).json({ ecotourismPlacesNearby });
+	} catch (err) {
+		console.error(err);
 		res.status(500).json({ error: err.message });
 	}
 });
